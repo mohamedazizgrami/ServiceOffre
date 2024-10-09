faa40e650f0efaeddf1e02d2e8edeae17d42bb03
Bady <grami.medaziz00@gmail.com>
2024-10-09 14:03:15 +0100

    fixing Demo

--- src/main/java/com/iset/demo/DemoApplication.java
+++ src/main/java/com/iset/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.iset.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
